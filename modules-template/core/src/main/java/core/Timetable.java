d48be7a197c7f36107a993fad450633eb34d3d92
Simen Seeberg-Rommetveit <simensee@stud.ntnu.no>
2021-09-20 13:35:22 +0200

    Implement structure Timetable.java

--- modules-template/core/src/main/java/core/Timetable.java
+++ modules-template/core/src/main/java/core/Timetable.java
@@ -2,4 +2,29 @@ package core;
 
 public class Timetable {
     
+    private final List<Event> eventList;
+
+    public Timetable() {
+        
+    }
+
+    // Return complete list with events
+    public getEventList() {
+        return null;
+    }
+
+    // Make new event in timetable
+    public void addEvent() { 
+
+    }
+
+    // Remove event from timetable
+    public Event removeEvent() {
+        return null;
+    }
+
+    
+    
+
+
 }
