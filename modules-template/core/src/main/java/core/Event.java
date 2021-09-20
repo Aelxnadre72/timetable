3a82c61fa652841f07f54d0e4ee837cf2afcbf4c
ninaatran <ninatra@stud.ntnu.no>
2021-09-20 13:36:41 +0200

    Implemented structure of Event.java

--- modules-template/core/src/main/java/core/Event.java
+++ modules-template/core/src/main/java/core/Event.java
@@ -1,5 +1,40 @@
 package core;
 
 public class Event {
+    private String eventName;
+    private LocalDateTime eventStart; //get day of week
+    private LocalDateTime eventEnd;
+    private String text;
     
+    public String getEventName(){
+        return eventName;
+    }
+
+    public String setEventName(String eventName){
+        this.eventName = eventName;
+    }
+
+    public String getText(){
+        return text;
+    }
+
+    public String setText(String text){
+        this.text = text;
+    }
+
+    public LocalDateTime getEventStart(){
+        return eventStart;
+    }
+
+    public LocalDateTime setEventStart(private LocalDateTime eventStart){
+        this.eventStart = eventStart;
+    }
+
+    public LocalDateTime getEventEnd(){
+        return eventEnd;
+    }
+
+    public LocalDateTime setEventEnd(private LocalDateTime eventEnd){
+        this.eventEnd = eventEnd;
+    }
 }
