3d11eadd1c1cadbde3d8cd1828f7d0d0ad2a1238
Alexander Le <alexle@stud.ntnu.no>
2021-09-21 17:09:16 +0200

    fixed time variables

--- modules-template/core/src/main/java/core/Event.java
+++ modules-template/core/src/main/java/core/Event.java
@@ -1,12 +1,14 @@
 package core;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Event {
     private String title;
     private String description;
-    private LocalDateTime timeStart; //getDayOfWeek(); function
-    private LocalDateTime timeEnd;
-    private LocalDateTime date;
+    private LocalTime timeStart; //getDayOfWeek(); function
+    private LocalTime timeEnd;
+    private LocalDate date;
     
     public String getTitle(){
         return title;
@@ -24,28 +26,59 @@ public class Event {
         this.description = description;
     }
 
-    public LocalDateTime getTimeStart(){
-        return timeStart;
+    public String getTimeStart(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        return timeStart.format(formatter);
     }
 
-    public void setTimeStart(String timeStart){
-        //this.timeStart = timeStart;
+    public void setTimeStart(String startString){
+        if(!isCorrectTimeFormat(startString)){
+            throw new IllegalArgumentException("setTimeStart has been given an argument of incorrect format.");
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        timeEnd = LocalTime.parse(startString, formatter);
     }
 
-    public LocalDateTime getTimeEnd(){
-        return timeEnd;
+    public String getTimeEnd(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        return timeEnd.format(formatter);
     }
 
-    public void setTimeEnd(String eventEnd){
-        //this.eventTimeEnd = eventTimeEnd;
+    public void setTimeEnd(String endString){
+        if(!isCorrectTimeFormat(endString)){
+            throw new IllegalArgumentException("setTimeEnd has been given an argument of incorrect format.");
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        timeEnd = LocalTime.parse(endString, formatter);
     }
 
-    public void setDate(String eventDate){
-       // this.eventDate = eventDate;
+    public void setDate(String dateString){
+        if(!isCorrectDateFormat(dateString)){
+            throw new IllegalArgumentException("setDate has been given an argument of incorrect format.");
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        date = LocalDate.parse(dateString, formatter);
     }
 
-    public LocalDateTime getDate(){
-        return date;
+    public String getDate(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return date.format(formatter);
+    }
+
+    public boolean isCorrectTimeFormat(String s){
+        // checks if String s is of length 5, contains a colon at index 2 and every other character is a digit
+        return s.length() == 5 && s.substring(2, 3).equals(":") && s.replace(":", "").matches("[0-9]+");
+    }
+
+    public boolean isCorrectDateFormat(String s){
+        // checks if String s is of length 10, contains a dot at index 2 and 5 and every other character is a digit
+        return s.length() == 10 && s.substring(2, 3).equals(".") && s.substring(5, 6).equals(".");
+    }
+
+    public static void main(String[] args) {
+        Event test = new Event();
+        test.setDate("21.02.2000");
+        System.out.println(test.getDate());
     }
 
 }
