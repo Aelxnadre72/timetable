3dbe3d4902954b273bf769f0f19b9d9b4c5e2e8e
Simen Seeberg-Rommetveit <simensee@stud.ntnu.no>
2021-09-21 17:20:34 +0200

    Make controller class

---
+++ modules-template/ui/src/main/java/ui/TimetableController.java
@@ -0,0 +1,19 @@
+package ui;
+
+public class TimetableController {
+    
+    public TimetableController() {
+        Timetable timetable = new
+    }
+
+    // Handle add event 
+    public void handleAdd() {
+        
+        updateView();
+    }
+
+    // Update GUI 
+    public void updateView() {
+        
+    }
+}
