216592e603ae7257cc961afda6079b299273af24
Simen Seeberg-Rommetveit <simensee@stud.ntnu.no>
2021-09-28 13:05:17 +0200

    Overwrite

--- modules-template/ui/src/main/java/ui/TimetableController.java
+++ modules-template/ui/src/main/java/ui/TimetableController.java
@@ -2,8 +2,6 @@ package ui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 import core.Event;
 import core.Timetable;
@@ -72,21 +70,6 @@ public class TimetableController  {
 
     public void updateTimetableView() {
         
-=======
-import javafx.fxml.FXML;
-
-public class TimetableController  {
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
-
-    @FXML
-    void initialize() {
-
->>>>>>> a485daaf196016f61b665349d3980ba2ecc4c7df
     }
 
     public boolean isAvailable(Event event) {
@@ -104,24 +87,3 @@ public class TimetableController  {
     public void handleRemove() {
     }
 }
-=======
-import javafx.fxml.FXML;
-
-public class TimetableController  {
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
-
-    @FXML
-    void initialize() {
-
-    }
-}
-
-<<<<<<< HEAD
->>>>>>> f6374443e88bb95aa39d5d5978d7359cefecea13
-=======
->>>>>>> a485daaf196016f61b665349d3980ba2ecc4c7df
