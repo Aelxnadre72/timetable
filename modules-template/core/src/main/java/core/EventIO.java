79b0f15f467e27ec2db2cff9725f6d0885b42467
Alexander Le <alexle@stud.ntnu.no>
2021-09-30 11:24:44 +0200

    fix

--- modules-template/core/src/main/java/core/EventIO.java
+++ modules-template/core/src/main/java/core/EventIO.java
@@ -1,13 +1,9 @@
 package core;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
