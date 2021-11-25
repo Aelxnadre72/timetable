bc243443969dc05667680bd90efa1864c10c51d3
ninaatran <ninatra@stud.ntnu.no>
2021-11-25 14:19:24 +0100

    Merged master into documatation branch. Ref #34

---
+++ timetable/core/src/test/java/timetable/json/TimetableModuleTest.java
@@ -0,0 +1,111 @@
+package timetable.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Iterator;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import timetable.core.User;
+import timetable.core.Timetable;
+import timetable.core.Event;
+
+/*{"timetables":[{"week":47,"year":2021,"events":[{"title":"study","category":"school","description":"exam","time-start":"12:00","time-end":"14:00","date":"25.11.2021"},{"title":"study2","category":"school","description":"exam","time-start":"10:00","time-end":"12:00","date":"26.11.2021"}]}]}*/
+public class TimetableModuleTest {
+    private static ObjectMapper mapper;
+
+    @BeforeAll
+    public static void setUp() {
+        mapper = TimetablePersistence.createMapper();
+    }
+
+    final static String timetableWithEvents = """
+        {
+            "timetables": [{"week":47, "year":2021,"events":[
+                {
+                    "title": "study",
+                    "category": "school",
+                    "description": "exam",
+                    "time-start": "12:00",
+                    "time-end": "14:00",
+                    "date": "25.11.2021"},
+                 {
+                    "title": "study2",
+                    "category": "school",
+                    "description": "exam",
+                    "time-start": "10:00",
+                    "time-end": "12:00",
+                    "date": "26.11.2021"}
+                    ]
+                }
+                ]
+            }
+        """;
+    static User createTimetableWithTwoEvents() {
+        User user = new User();
+        Timetable timetable = new Timetable(47, 2021);
+        user.addTimetable(timetable);
+        Event ev = new Event("study", "school", "exam", "12:00", "14:00", "25.11.2021");
+        Event ev1 = new Event("study2", "school", "exam", "10:00", "12:00", "26.11.2021");
+        timetable.addEvent(ev);
+        timetable.addEvent(ev1);
+        return user;
+    }
+
+    @Test
+    public void testSerializers() {
+        User user = createTimetableWithTwoEvents();
+        try {
+            assertEquals(timetableWithEvents.replaceAll("\\s+", ""), mapper.writeValueAsString(user));
+        } catch (JsonProcessingException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testDeserializers() {
+        try {
+            User user = mapper.readValue(timetableWithEvents, User.class);
+            Iterator<Timetable> timetables = user.getTimetableList().iterator();
+            assertTrue(timetables.hasNext());
+
+            Timetable timetable = timetables.next();
+            assertEquals(47,timetable.getWeek());
+
+            Iterator<Event> events = timetable.getEventList().iterator();
+            assertTrue(events.hasNext());
+            
+        } catch (JsonProcessingException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test 
+    public void testSerializersAndDeserializers() {
+        User user = new User();
+        Timetable timetable = new Timetable(47, 2021);
+        user.addTimetable(timetable);
+        Event ev = new Event("study", "school", "exam", "12:00", "14:00", "25.11.2021");
+        Event ev1 = new Event("study2", "school", "exam", "10:00", "12:00", "26.11.2021");
+        timetable.addEvent(ev);
+        timetable.addEvent(ev1);
+
+        try {
+            String json = mapper.writeValueAsString(user);
+            User user1 = mapper.readValue(json, User.class);
+            Iterator<Timetable> timetables = user1.getTimetableList().iterator();
+            assertTrue(timetables.hasNext());
+
+            Timetable timetable1 = timetables.next();
+            assertEquals(47, timetable1.getWeek());
+
+            Iterator<Event> events = timetable.getEventList().iterator();
+            assertTrue(events.hasNext());
+        } catch (JsonProcessingException e) {
+            fail(e.getMessage());
+        }
+    }
+}
---
+++ timetable/core/src/test/java/timetable/json/TimetablePersistenceTest.java
@@ -0,0 +1,90 @@
+package timetable.json;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Iterator;
+import org.junit.jupiter.api.Test;
+import timetable.core.User;
+import timetable.core.Timetable;
+import timetable.core.Event;
+
+
+
+public class TimetablePersistenceTest {
+    private TimetablePersistence timetablePersistence = new TimetablePersistence();
+
+    private User createSamplUser(){
+        User user = new User();
+        Timetable timetable = new Timetable(47, 2021);
+        Event event1 = new Event("study", "school", "exam", "12:00", "14:00", "25.11.2021");
+        Event event2 = new Event("study2", "school", "exam", "10:00", "12:00", "26.11.2021");
+        timetable.addEvent(event1);
+        timetable.addEvent(event2);
+        user.addTimetable(timetable);
+        timetable.removeEvent(event1);
+        //user.getTimetable("1");
+        //user.hasTimetable("1");
+
+        return user;
+    }
+
+    
+    private void checkSampleUser(User user, User user1){
+        Iterator<Timetable> time = user.getTimetableList().iterator();
+        Iterator<Timetable> time1 = user1.getTimetableList().iterator();
+        assertTrue(time.hasNext());
+        assertTrue(time1.hasNext());
+
+ 
+        Timetable timetable = time.next();
+        Timetable timetable1 = time1.next();
+        assertEquals(timetable.getWeek(), timetable1.getWeek());
+        Iterator<Event> ev = timetable.getEventList().iterator();
+        Iterator<Event> ev1 = timetable1.getEventList().iterator();
+        assertTrue(ev.hasNext());
+        assertTrue(ev1.hasNext());
+
+    }
+
+    @Test
+    public void testSerializersDeserializers_StringWriter() {
+        User user = createSamplUser();
+        try{
+            StringWriter writer = new StringWriter();
+            timetablePersistence.writeUser(user, writer);
+            String json = writer.toString();
+            User user1 = timetablePersistence.readUser(new StringReader(json));
+            checkSampleUser(user, user1);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testSerializersDeserializers_SaveFiles() {
+        User user = createSamplUser();
+        
+        timetablePersistence.setFilePath("user-" + System.currentTimeMillis() + ".json");
+        Path savePath = timetablePersistence.getSaveFilePath();
+        try {
+            timetablePersistence.saveUser(user);
+            assertTrue(Files.exists(savePath));
+            User user1 = timetablePersistence.loadUser();
+            checkSampleUser(user, user1);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        } finally {
+            try {
+                Files.deleteIfExists(savePath);
+            } catch (IOException e) {
+
+            }
+        }
+    }
+}
