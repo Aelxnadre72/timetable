41af8aa448c838392df1931d6b2501bb45a07587
Elise Moland <elismol@stud.ntnu.no>
2021-11-27 01:20:14 +0100

    resolved checkstyle for pull from origin master #42

--- timetable/fxui/src/main/java/timetable/ui/App.java
+++ timetable/fxui/src/main/java/timetable/ui/App.java
@@ -12,26 +12,26 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
-    /**
-     * Method for testing
-     */
-    public static void supportHeadless() {
-        if (Boolean.getBoolean("headless")) {
-            System.setProperty("testfx.robot", "glass");
-            System.setProperty("testfx.headless", "true");
-            System.setProperty("prism.order", "sw");
-            System.setProperty("prism.text", "t2k");
-            System.setProperty("java.awt.headless", "true");
-          }      
-    }
+  /**
+  * Method for testing.
+  */
+  public static void supportHeadless() {
+    if (Boolean.getBoolean("headless")) {
+      System.setProperty("testfx.robot", "glass");
+      System.setProperty("testfx.headless", "true");
+      System.setProperty("prism.order", "sw");
+      System.setProperty("prism.text", "t2k");
+      System.setProperty("java.awt.headless", "true");
+    }      
+  }
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/timetable/ui/App.fxml"));
-        Parent parent = fxmlLoader.load();
-        stage.setScene(new Scene(parent));
-        stage.show();
-    }
+  @Override
+  public void start(Stage stage) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/timetable/ui/App.fxml"));
+    Parent parent = fxmlLoader.load();
+    stage.setScene(new Scene(parent));
+    stage.show();
+  }
 
   public static void main(String[] args) {
     launch();
